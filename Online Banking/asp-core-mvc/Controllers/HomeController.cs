376f6375a1d5c417a0717a9fc77b9ba5321439ee
David Tran <37198403+trantheman@users.noreply.github.com>
2019-10-01 08:55:00 -0500

    Updated Home Page

--- Online Banking/asp-core-mvc/Controllers/HomeController.cs
+++ Online Banking/asp-core-mvc/Controllers/HomeController.cs
@@ -13,28 +13,52 @@ namespace asp_core_mvc.Controllers
     {
         public IActionResult Index()
         {
+            List<Alerts> alerts = new List<Alerts>();
+            List<Transactions> trans = new List<Transactions>();
+
+            string connStr = "server=localhost;user=root;database=cs451_project;port=3306;password=123456";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+            string sql = "SELECT * FROM Alerts";
+            string sql2 = "SELECT * FROM Transactions";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlCommand cmd2 = new MySqlCommand(sql2, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Alerts alert = new Alerts(
+                    rdr.GetString(0), // tdate
+                    rdr.GetString(1) // alertreason
+                );
+                alerts.Add(alert);
+            }
+            rdr.Close();
+            MySqlDataReader rdr2 = cmd2.ExecuteReader();
+            while (rdr2.Read())
+            {
+                Transactions transac = new Transactions(
+                    rdr2.GetInt32(0), // tid
+                    rdr2.GetString(1), // tdate
+                    rdr2.GetString(2), // tdesc
+                    rdr2.GetString(3), // lo
+                    rdr2.GetDouble(4), // amnt
+                    rdr2.GetDouble(5) // bal
+                );
+                trans.Add(transac);
+            }
+            rdr2.Close();
+            conn.Close();
+
+            ViewBag.AlertsData = alerts;
+            ViewBag.TransactionsData = trans;
+
             return View();
         }
 
-        public IActionResult Transactions()
-        {
-            return View();
-        }
-
-        public IActionResult Alerts()
-        {
-            return View();
-        }
-
-        public IActionResult Rules()
-        {
-            return View();
-        }
-
-        public IActionResult Reports()
-        {
-            return View();
-        }
+        public IActionResult Transactions() { return View(); }
+        public IActionResult Alerts() { return View(); }
+        public IActionResult Rules() { return View(); }
+        public IActionResult Reports() { return View(); }
 
         public IActionResult About()
         {
