056a66b490220a43eeaff209f781ac14ccefbf88
Jonathan <jawhf4@mail.umkc.edu>
2019-09-30 22:41:17 -0500

    Initial Project Commit
    Created a base project with only a homepage with a welcome text

---
+++ Commerce Online Banking/Models/DatabaseHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commerce_Online_Banking.Models
+{
+    public class DatabaseHandler
+    {
+    }
+}
