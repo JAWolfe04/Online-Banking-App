bcc90043732a4f11f867b2ca2d9d559cacce7fdf
Jonathan <jawhf4@mail.umkc.edu>
2019-10-14 16:31:33 -0500

    Added security measures and alerts trigger
    Changed password field in login from a text field to a password to hide entries
    Removed transaction IDs from any lists for security
    Changed some SQL attribute names for a consistent naming
    Added transaction info to alert page

--- Online Banking/asp-core-mvc/Models/Alerts.cs
+++ Online Banking/asp-core-mvc/Models/Alerts.cs
@@ -6,6 +6,11 @@ namespace asp_core_mvc.Models
     {
         public int TransId { get; set; }
         public string TransDate { get; set; }
+        public string TransDesc { get; set; }
+        public string TransType { get; set; }
+        public string Location { get; set; }
+        public double Amount { get; set; }
+        public double Balance { get; set; }
         public string AlertReason { get; set; }
     }
 }
